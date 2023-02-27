5ecc8028420f48a1104b7f3165e9b93b05835bb6
Joshua Turner <43835483+Jawshuwa@users.noreply.github.com>
2023-02-26 16:11:46 -0800

    Milestone 2, CST150

---
+++ Milestone_Stage2/DriverMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InventorySystem
+{
+    public class MyMainClass
+    {
+        static void Main()
+        {
+            // Create a test instance of the Inventory Class, with  the constructor
+            Inventory test = new Inventory();
+            // Set the fields to new values
+            test.QuantityOnHand = 104; test.QuantityOrdered = 500; test.ProductNumber = 01;
+            test.ProductName = "Milk Carton"; test.PurchasePrice = 1.49; test.SellPrice = 3.99;
+
+            // Call the methods
+            double method1Test = test.CalculateRevenue();
+            double method2Test = test.CalculateRevenue(150);
+            double method3Test = test.ApplyDiscount(20);
+
+            // Testing the class properties and methods by writing them to the console
+            Console.WriteLine("Quantity on Hand: " + test.QuantityOnHand);
+            Console.WriteLine("Quantity Ordered: " + test.QuantityOrdered);
+            Console.WriteLine("Product Number: " + test.ProductNumber);
+            Console.WriteLine("Product Name: " + test.ProductName);
+            Console.WriteLine("Purchase Price: " + test.PurchasePrice);
+            Console.WriteLine("Sell Price: " + test.SellPrice);
+            Console.WriteLine("Calculate Revenue No Parameter:  " + method1Test);
+            Console.WriteLine("Calculate Revenue with Parameter: " + method2Test);
+            Console.WriteLine("Apply Discount: " + method3Test);
+
+        }
+    }
+}
---
+++ Milestone_Stage2/Inventory.cs
@@ -0,0 +1,60 @@
+﻿// Joshua Turner, CST-150, this is my own work
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem
+{
+    class Inventory
+    {
+        // Private fields for inventory items
+        private int _quantityonhand;
+        private int _quantityordered;
+        private int _productnnumber;
+        private string _productname;
+        private double _purchaseprice;
+        private double _sellprice;
+
+        // Constructor, with default values of 0 or empty
+        public Inventory()
+        {
+            _quantityonhand = 0;
+            _quantityordered = 0;
+            _productnnumber = 0;
+            _productname = string.Empty;
+            _purchaseprice = 0;
+            _sellprice = 0;
+        }
+
+        // Properties for the fields
+        public int QuantityOnHand { get { return _quantityonhand; } set { _quantityonhand = value; } }
+        public int QuantityOrdered { get { return _quantityordered; } set { _quantityordered = value; } }
+        public int ProductNumber { get { return _productnnumber; } set { _productnnumber = value; } }
+        public string ProductName { get { return _productname; } set { _productname = value; } }
+        public double PurchasePrice { get { return _purchaseprice; } set { _purchaseprice = value; } }
+        public double SellPrice { get { return _sellprice; } set { _sellprice = value; } }
+
+        // Methods
+        public double CalculateRevenue()
+        {
+            // Returns base revenue from an item sale
+            return SellPrice - PurchasePrice;
+        }
+
+        public double CalculateRevenue(int saleNumber)
+        {
+            // Returns base revenue for many item sales of the object
+            return saleNumber * (SellPrice - PurchasePrice);
+        }
+
+        public double ApplyDiscount(double percent)
+        {
+            // Takes the parameter and converts it to a decimal to calculate a discounted sell price
+            double dicount = percent / 100;
+            return SellPrice - (SellPrice * dicount);
+        }
+    }
+}
+
